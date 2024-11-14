0830b225a4230fed9b51965b9760dd1cfabb5ad3
Anoop mishra <anoop.mishra1@pearson.com>
2024-11-14 16:17:28 +0530

    fixed keyboard

--- src/pages/games/typingGame/components/keyBoard.js
+++ src/pages/games/typingGame/components/keyBoard.js
@@ -8,6 +8,9 @@ const KeyboardContainer = styled.div`
   margin-top: 20px;
   border: 1px solid #ada1a1;
   border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, 
+              rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, 
+              rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 `;
 
 const KeyboardRow = styled.div`
@@ -20,7 +23,7 @@ const Key = styled.button`
   width: ${({ isWide }) => (isWide ? '100px' : '50px')};
   height: 50px;
   margin: 3px;
-  border: 1px solid #000;
+  border: 1px solid #ada1a1;
   border-radius: 5px;
   background-color: #f1f1f1;
   display: flex;
@@ -28,6 +31,9 @@ const Key = styled.button`
   justify-content: center;
   cursor: pointer;
   transition: background-color 0.2s, transform 0.1s;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, 
+              rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, 
+              rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 
   ${({ isActive }) =>
     isActive &&
@@ -57,14 +63,19 @@ function Keyboard({ keyGroups = [] }) {
   };
 
   const handleKeyDown = (event) => {
-    const key = event.key;
+    let key = event.key;
+    // Normalize the key for alphabetic characters
+    if (key.length === 1) {
+      key = key.toUpperCase(); // This ensures both 'a' and 'A' are treated the same
+    }
+
     if (Object.values(keys).flat().includes(key)) {
-      setActiveKey(key);
+      setActiveKey(key); // Set active key on keydown
     }
   };
 
   const handleKeyUp = () => {
-    setActiveKey(null);
+    setActiveKey(null); // Deselect key on keyup
   };
 
   useEffect(() => {
