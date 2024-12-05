873c00fa7a35ebc29ef98e2bcc60621ad937339a
Anoop mishra <anoop.mishra1@pearson.com>
2024-12-05 22:07:23 +0530

    adding broadcast test1

---
+++ src/test-broadcast/src/pages/mobileAudioReciver/audioBroadcastReciever.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react'
+import io from 'socket.io-client';
+const socket = io('http://192.168.0.201:4000'); 
+
+const AudioBroadcastReciever = () => {
+  const audioRef = useRef(null);
+  const audioChunks = useRef([]);
+
+  useEffect(() => {
+    // Listen for the 'audio' event from the server
+    socket.on('audio', (audioData) => {
+      // Append the received audio chunk to the audioChunks array
+      audioChunks.current.push(audioData);
+
+      // Create a Blob from the audio chunks
+      const audioBlob = new Blob(audioChunks.current, { type: 'audio/webm' });
+      const audioURL = URL.createObjectURL(audioBlob);
+      if (audioRef.current) {
+        audioRef.current.src = audioURL;
+        audioRef.current.play();
+      }
+    });
+    return () => {
+      socket.off('audio');
+      socket.off('new-message');
+    };
+  }, []);
+
+  return (
+    <div>
+       <h1>Real-Time Audio Streaming</h1>
+      <h2>Audio Player</h2>
+      <audio ref={audioRef} controls autoPlay>
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  )
+}
+
+export default AudioBroadcastReciever
